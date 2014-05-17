ba4574ac3432c352a368aa64531a99984a7bbcfc
Derek Jensen <derek.jensen5@gmail.com>
2014-05-17 15:26:49 -0600

    Release Info

--- 7DTD Remote Server Manager/Properties/AssemblyInfo.cs
+++ 7DTD Remote Server Manager/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("7DTD Remote Server Manager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A 7DTD Remoter Server Managing Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("7DTD Remote Server Manager")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyFileVersion("0.0.0.*")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
